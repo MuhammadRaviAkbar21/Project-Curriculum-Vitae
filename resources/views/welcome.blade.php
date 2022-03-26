9ff7e714f85ef4260d077b00e15de16f8accc2e8
Muhammad Ravi Akbar <ravi.akbar.2000@gmail.com>
2022-03-26 15:29:54 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,335 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Curriculum Vitae</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="vendor/devicons/css/devicons.min.css" rel="stylesheet">
+    <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/resume.min.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <span class="d-block d-lg-none">Start Bootstrap</span>
+        <span class="d-none d-lg-block">
+          <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/profile.jpg" alt="">
+        </span>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#experience">Experience</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#education">Education</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#skills">Skills</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#hobby">Hobby</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#languages">Languages</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container-fluid p-0">
+
+      <section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
+        <div class="my-auto">
+          <h1 class="mb-0">Muhammad Ravi Akbar
+            <span class="text-primary"></span>
+          </h1>
+          <div class="subheading mb-5">Indonesia, Sumatra Barat,
+            Padang Panjang · +62 0822 7821 5887·
+            <a href="mailto:name@email.com">ravi.akbar.2000@gmail.com</a>
+          </div>
+          <p class="mb-5">Nama lengkap Saya Muhammad Ravi Akbar dengan nama
+            panggilan Ravi dan Saya sekarang berumur 21 tahun. Latar
+            belakang pendidikan saya sekarang adalah seorang mahasiswa
+            yang sedang menempuh pendidikan di Politeknik Negeri Padang
+            jurusan Teknologi Informasi, Program Studi Manajemen Informatika</p>
+          <ul class="list-inline list-social-icons mb-0">
+            <li class="list-inline-item">
+              <a href="#">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
+                </span>
+               </a>
+            </li>
+            <li class="list-inline-item">
+                <a href="#">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-whatsapp fa-stack-1x fa-inverse"></i>
+                </span>
+               </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="experience">
+        <div class="my-auto">
+          <h2 class="mb-5">Experience</h2>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">VSGA Design Training </h3>
+              <div class="subheading mb-3">Intelitec Solutions</div>
+              <p>Saya pernah mengikuti pelatihan desain grafis yang
+                diselenggarakan oleh Digital Talent Scholarship Program
+                dengan Program DTS 2021 yaitu Vocational School Graduate
+                Academy (VSGA). </p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">August 2021 - October 2021</span>
+            </div>
+          </div>
+
+        </div>
+
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="education">
+        <div class="my-auto">
+          <h2 class="mb-5">Education</h2>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">SD Negeri 06 Balai Balai</h3>
+              <div class="subheading mb-3">Sekolah Dasar</div>
+              <div>Saya pernah bersekolah di SD Negeri 06
+                Balai Balai, Padang Panjang. </div>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">July 2008 - June 2013</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">SMP Negeri 1 Padang Panjang</h3>
+              <div class="subheading mb-3">Sekolah Menengah Pertama</div>
+              <div>Saya pernah bersekolah di SMP Negeri 1
+                Padang Panjang. </div>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">July 2013 - June 2016</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">SMK Negeri 2 Padang Panjang</h3>
+              <div class="subheading mb-3">Multimedia</div>
+              <div>Saya pernah bersekolah di SMK Negeri 2
+                Padang Panjang dengan jurusan Multimedia. </div>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">July 2016 - June 2019</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Politeknik Negeri Padang</h3>
+              <div class="subheading mb-3">Teknologi Informasi</div>
+              <div>Saya kuliah di Politeknik Negeri Padang jurusan
+                Teknologi Informasi, Program Studi Manajemen
+                Informatika. </div>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">July 2019 - March 2022</span>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="skills">
+        <div class="my-auto">
+          <h2 class="mb-5">Skills</h2>
+
+          <div class="subheading mb-3">Programming Languages &amp; Tools</div>
+          <ul class="list-inline list-icons">
+            <li class="list-inline-item">
+              <i class="devicons devicons-html5"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-css3"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-javascript"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-bootstrap"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-java"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-laravel"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-php"></i>
+            </li>
+          </ul>
+
+          <div class="subheading mb-3">Graphic Designer &amp; Tools</div>
+          <ul class="list-inline list-icons">
+            <li class="list-inline-item">
+              <i class="devicons devicons-photoshop"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-illustrator"></i>
+            </li>
+          </ul>
+
+          <div class="subheading mb-3">Skills</div>
+          <ul class="fa-ul mb-0">
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Web Design</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Java</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              JavaScript</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              HTML</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Bootstrap</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              PHP</li>
+            <li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Laravel</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Photoshop</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Illustrator</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              CorelDraw</li>
+          </ul>
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="hobby">
+        <div class="my-auto">
+          <h2 class="mb-5">Hobby</h2>
+          <ul class="fa-ul mb-0">
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Reading</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Gaming</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Badminton</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Basketball</li>
+          </ul>
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="languages">
+        <div class="my-auto">
+          <h2 class="mb-5">Languages</h2>
+          <ul class="fa-ul mb-0">
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Indonesia</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              English</li>
+            <li>
+          </ul>
+        </div>
+      </section>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/resume.min.js"></script>
+
+  </body>
+
+</html>
